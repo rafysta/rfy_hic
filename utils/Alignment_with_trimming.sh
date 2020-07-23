5c28d9b7157683b73c96342d3347c8b45bf703e6
hideki <rafysta@gmail.com>
2020-07-23 11:11:41 -0700

    Update: terminate program if Bowtie2 get error

--- utils/Alignment_with_trimming.sh
+++ utils/Alignment_with_trimming.sh
@@ -9,14 +9,20 @@ DIR_LIB=$(dirname $0)
 PROGRAM_CIGAR=${DIR_LIB}/CigarFilter.pl
 VERBOSE=${VERBOSE:-FALSE}
 DIR_tmp=$(mktemp -d ${DIR_tmporary}/tmp_${OUT}.XXXXX)
+trap "rm ${DIR_tmp}" 0
 FILE_log=${DIR_DATA}/${OUT}_bowtie2.log
 FILE_tmp=${DIR_tmp}/${OUT}_bowtie2_tmp.log
-
 
 echo "Length Total % NoAlign % Unique % Multiple %" | tr ' ' '\t' > $FILE_log
 function getLog(){
 	[ $VERBOSE = "TRUE" ] && cat $FILE_tmp
 	cat $FILE_tmp | tr -d '()' | awk -v OFS='\t' 'NR>1&&NR<6{print $1,$2}' | xargs | tr ' ' '\t' >> $FILE_log && rm $FILE_tmp
+}
+function getError(){
+	echo "Bowtie2 failed for the alignment while $1"
+	mv $FILE_tmp ${DIR_DATA}/${OUT}_bowtie2_error.log
+	rm -rf ${DIR_tmp}
+	exit 1
 }
 
 zcat $(echo ${FILE_fastq} | tr ',' ' ') > ${DIR_tmp}/input.fastq
@@ -30,7 +36,7 @@ let MAX_TRIM=$READ_LENGTH-20
 
 [ $VERBOSE = "TRUE" ] && echo "Align entire ${READ_LENGTH}bp read"
 echo -n "${READ_LENGTH}bp	" >> $FILE_log
-bowtie2 -x ${BOWTIE2_INDEX} -U input.fastq -q -p 12 --no-unal --un ${OUT}_unaligned.fastq > ${OUT}.sam 2> $FILE_tmp && getLog
+bowtie2 -x ${BOWTIE2_INDEX} -U input.fastq -q -p 12 --no-unal --un ${OUT}_unaligned.fastq > ${OUT}.sam 2> $FILE_tmp && getLog || getError "first ${READ_LENGTH}bp"
 mv ${OUT}_unaligned.fastq ${OUT}_tmp.fastq
 
 
@@ -38,13 +44,13 @@ for LEN in $(seq 5 5 $MAX2_TRIM)
 do
 	[ $VERBOSE = "TRUE" ] && echo && echo "Trim ${LEN}bp and align"
 	echo -n "Trim ${LEN}bp	" >> $FILE_log
-	bowtie2 -x ${BOWTIE2_INDEX} -U ${OUT}_tmp.fastq -q -3 $LEN -p 12 --no-hd --no-unal --un ${OUT}_unaligned.fastq >> ${OUT}.sam 2> $FILE_tmp && getLog
+	bowtie2 -x ${BOWTIE2_INDEX} -U ${OUT}_tmp.fastq -q -3 $LEN -p 12 --no-hd --no-unal --un ${OUT}_unaligned.fastq >> ${OUT}.sam 2> $FILE_tmp && getLog || getError "Trim ${LEN}bp"
 	mv ${OUT}_unaligned.fastq ${OUT}_tmp.fastq
 done
 
 [ $VERBOSE = "TRUE" ] && echo && echo "Trim ${MAX_TRIM}bp and align"
 echo -n "Trim ${MAX_TRIM}bp	" >> $FILE_log
-bowtie2 -x ${BOWTIE2_INDEX} -U ${OUT}_tmp.fastq -q -3 ${MAX_TRIM} -p 12 --no-hd  >> ${OUT}.sam 2> $FILE_tmp && getLog
+bowtie2 -x ${BOWTIE2_INDEX} -U ${OUT}_tmp.fastq -q -3 ${MAX_TRIM} -p 12 --no-hd  >> ${OUT}.sam 2> $FILE_tmp && getLog || getError "Trim ${MAX_TRIM}bp"
 rm ${OUT}_tmp.fastq
 
 perl ${PROGRAM_CIGAR} -s ${OUT}.sam -o ${OUT}_fastqList.txt > ${OUT}_tmp.sam
@@ -57,7 +63,7 @@ do
 	# echo "Command : bowtie2 -x ${BOWTIE2_INDEX} -U ${FASTQ_REANALYZE} -q ${TRIM_OPTION} -p 12 --no-hd"
 	[ $VERBOSE = "TRUE" ] && echo && echo "Bowtie2 option: ${TRIM_OPTION} and align"
 	echo -n "Option:${TRIM_OPTION}	" >> $FILE_log
-	bowtie2 -x ${BOWTIE2_INDEX} -U ${FASTQ_REANALYZE} -q ${TRIM_OPTION} -p 12 --no-hd >> ${OUT}.sam 2> $FILE_tmp && getLog
+	bowtie2 -x ${BOWTIE2_INDEX} -U ${FASTQ_REANALYZE} -q ${TRIM_OPTION} -p 12 --no-hd >> ${OUT}.sam 2> $FILE_tmp && getLog || getError "option ${TRIM_OPTION}"
 	rm ${FASTQ_REANALYZE}
 done
 rm ${OUT}_fastqList.txt
